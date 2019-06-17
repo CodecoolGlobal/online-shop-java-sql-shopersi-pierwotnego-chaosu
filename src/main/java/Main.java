7d759757ad1addd74dd6dc62419d53e41baa44f7
David <cichy.dawid@gmail.com>
2019-06-17 13:23:17 +0200

    database integration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,29 @@
+import java.sql.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db");
+            System.out.println("Opened database successfully");
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCTS;");
+            while(rs.next()){
+                String name = rs.getString("name");
+                System.out.println(name);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+
+
 }
+}
+
