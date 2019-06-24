b9788f48903b89e5b8175c60dd1b12720dde4f8c
PrzemekKondziela <kondziela.p@gmail.com>
2019-06-24 11:02:30 +0200

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,62 +7,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Product> list = new ProductDAO().read();
 
-        System.out.println(list);
-
-        ArrayList<Product> list2 = new ProductDAO().read();
-
-        System.out.println(list2);
-    }
-
-
-    public static int getCategoryId(String category){
-
-        Connection c = null;
-
-        int colId=0;
-
-        try {
-            c = new DataSource().getConnection();
-            System.out.println("Opened database successfully");
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM CATEGORIES where NAME = \'"+ category + "\' ");
-            while(rs.next()){
-                colId = rs.getInt("id");
-                System.out.println(colId);
-            }
-            rs.close();
-            stmt.close();
-            c.close();
-
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-
-        return colId;
-    }
-
-    public static void testing(){
-        Connection c = null;
-
-        try {
-            c = new DataSource().getConnection();
-            System.out.println("Opened database successfully");
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCTS;");
-            while(rs.next()){
-                String name = rs.getString("name");
-                System.out.println(name);
-            }
-            rs.close();
-            stmt.close();
-            c.close();
-
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
     }
 }
--- src/main/java/controler/CustomerControler.java
+++ src/main/java/controler/CustomerControler.java
@@ -1,54 +1,6 @@
 package controler;
 
-import model.shop.Basket;
-import model.shop.Product;
 
 public class CustomerControler {
-    public void addProductToBasket(Product product, Basket basket) {
 
-        /*
-        TODO Co tu zrobić
-         */
-
-    }
-
-    public void editProductQuantityInBasket() {
-
-    }
-
-    public void deleteProductFromBasket() {
-
-    }
-
-    public void getProductsFromBasket() {
-
-    }
-
-    public void placeAnOrder() {
-
-    }
-
-    public void getPreviousOrder() {
-
-    }
-
-    public void getAllAvailableProductsWithTheirRates() {
-
-    }
-
-    public void getListOfProductsFromCategory() {
-
-    }
-
-    public void checkAvailabilityOfProduct() {
-
-    }
-
-    public void rateProductAfterReceivingOrderWithFiveStarScale() {
-
-    }
-
-    public void getStatisticsOfOrders() {
-
-    }
 }
