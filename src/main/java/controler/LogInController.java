7b5b9ab4ce76a9b94ef393477848b079036e078e
Kamil Kiklica <kamil.kiklica@gmail.com>
2019-06-26 17:49:41 +0200

    add newAccount feature

--- src/main/java/controler/LogInController.java
+++ src/main/java/controler/LogInController.java
@@ -20,9 +20,11 @@ public class LogInController {
             switch (scanner.nextInt()) {
                 case 1: {
                     logInUser();
+                    break;
                 }
 
                 case 2: {
+                    createNewAccount();
                     break;
                 }
 
@@ -42,22 +44,56 @@ public class LogInController {
         Scanner scanner = new Scanner(System.in);
         System.out.print("User name: ");
         String name = scanner.next();
-        System.out.print("User password: ");
-        String password = scanner.next();
-        User user = new UsersDAO().readUser(name,password);
-        if (user.getName()!=null){
-            if (user.getAdmin() == 1){
-                AdminController adminController = new AdminController(user);
-                adminController.run();
-            } else {
-                CustomerController customerController = new CustomerController(user);
-                customerController.run();
+        if (new UsersDAO().readUser(name).getName()==null){
+            System.out.println();
+            System.out.println("There is no such user in the database.");
+            System.out.println();
+        } else {
+            System.out.print("User password: ");
+            String password = scanner.next();
+            User user = new UsersDAO().readUser(name);
+            if (user.getName() != null && !user.getPassowrd().equals(password)) {
+                System.out.println();
+                System.out.println("Wrong password... ");
+                System.out.println();
+
+            }
+            if (user.getName() != null && user.getPassowrd().equals(password)) {
+                if (user.getAdmin() == 1) {
+                    AdminController adminController = new AdminController(user);
+                    adminController.run();
+                } else {
+                    CustomerController customerController = new CustomerController(user);
+                    customerController.run();
+                }
             }
         }
-
-
-
     }
+
+    public void createNewAccount(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose your username: ");
+        String name = scanner.next();
+        User newUser = new UsersDAO().readUser(name);
+        while (newUser.getName()!=null) {
+            System.out.println();
+            System.out.println("These username already exist. Choose another one:");
+            System.out.println();
+            name = scanner.next();
+            newUser = new UsersDAO().readUser(name);
+
+        }
+        newUser.setAdmin(0);
+        newUser.setName(name);
+        System.out.print("Choose your password: ");
+        String password = scanner.next();
+        newUser.setPassowrd(password);
+        new UsersDAO().create(newUser);
+        System.out.println();
+        System.out.println("You have created new account correctly. You can log in now.");
+        System.out.println();
+    }
+
 
     // User newUser = new UsersDAO().readUser("Dejw","dejw");
     //           System.out.println(newUser.getName());
---
+++ src/main/java/model/shop/Orders.java
@@ -0,0 +1,10 @@
+package model.shop;
+
+import java.time.LocalDate;
+
+public class Orders {
+    int id;
+    int userId;
+    String status;
+    LocalDate date;
+}
