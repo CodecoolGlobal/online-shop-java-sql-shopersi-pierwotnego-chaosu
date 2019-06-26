85815d7823ad7c5a2fa887b5e8ad955a40b80520
David <cichy.dawid@gmail.com>
2019-06-26 13:49:59 +0200

    Add ProductList class

---
+++ src/main/java/model/shop/lists/ProductList.java
@@ -0,0 +1,32 @@
+package model.shop.lists;
+
+import model.shop.Product;
+
+import java.util.ArrayList;
+
+public class ProductList {
+    ArrayList<Product> products;
+
+    public ProductList(){}
+
+    public ProductList(ArrayList<Product> products){
+        this.products = products;
+    }
+
+    public void add(Product product){
+        this.products.add(product);
+    }
+
+    public void removeFromList(Product product){
+        products.remove(product);
+    }
+
+    public void delete(Product product){
+        removeFromList(product);
+        product.delete();
+    }
+
+    public ArrayList<Product> getProducts(){
+        return this.products;
+    }
+}
