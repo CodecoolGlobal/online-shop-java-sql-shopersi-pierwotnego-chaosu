9c4e30a45d48656bffa4d911d49f8142e6f09bcc
David <cichy.dawid@gmail.com>
2019-07-01 13:26:15 +0200

    Finding Product in list by id

--- src/main/java/model/shop/lists/ProductList.java
+++ src/main/java/model/shop/lists/ProductList.java
@@ -3,9 +3,10 @@ package model.shop.lists;
 import model.shop.Product;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductList {
-    ArrayList<Product> products;
+    List<Product> products;
 
     public ProductList() {
     }
@@ -27,9 +28,39 @@ public class ProductList {
         product.delete();
     }
 
-    public ArrayList<Product> getProducts() {
+    public List<Product> getProducts() {
         return this.products;
     }
+
+    public boolean isIdValid(int id){
+        //checks if product with given id exists
+        for (Product product: products
+             ) { if (product.getId() == id) return true;
+        }
+        return false;
+    }
+
+    public Product getProductById(int id){
+        //returns the product of given id
+        for (Product product: products
+        ) { if (product.getId() == id) return product;
+        }
+        return null;
+    }
+
+    public void removeAmountById(int id, int amount){
+        //removes amount from product of given id
+        Product product = getProductById(id);
+        product.setAmount(product.getAmount()-amount);
+    }
+
+    public void updateAllProductsInDataBase(){
+        for (Product product: products
+        ) { product.update();
+        }
+    }
+
+    //updateAllProducts
 
     @Override
     public String toString() {
